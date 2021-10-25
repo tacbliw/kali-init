22bb16f37147daac004bf036adbeb4ccef56bb47
wildcat <letranhaitung@gmail.com>
2021-10-26 05:44:34 +0700

    changes

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ cp ./vimrc /usr/share/vim/vimrc
 
 # apt stuff, run upgrade first
 #apt install -y --reinstall open-vm-tools-desktop
-apt install -y python3-pip checksec gdb libmpc-dev strace elfutils gdbserver 1>/dev/null
+apt install -y python3-pip checksec gdb libmpc-dev strace elfutils gdbserver gdb-multiarch ropper checksec || exit 1
 
 # Python
 echo "Setting up Python..."
@@ -35,11 +35,11 @@ rm -rf /bin/python
 ln -s /bin/python3 /bin/python
 python --version
 
-pip3 install pwn pwntools angr PyCrypto z3-solver gmpy2 1>/dev/null
+pip3 install pwn pwntools angr pycryptodome z3-solver gmpy2 || exit 1
 
 # pwninit
 echo "Setting up pwninit..."
-wget -O "/bin/pwninit" "https://github.com/io12/pwninit/releases/download/2.2.0/pwninit"
+wget -O "/bin/pwninit" "https://github.com/io12/pwninit/releases/download/3.0.2/pwninit"
 chmod +x /bin/pwninit
 
 if [ -e ~/.zshrc ]; then
@@ -56,29 +56,6 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 
 sudo apt-get update
 sudo apt-get install sublime-text
-
-cat << EOF >> /etc/hosts
-127.0.0.1 www.sublimetext.com
-127.0.0.1 license.sublimehq.com
-EOF
-
-echo "++++++++++++ KEY ++++++++++++"
-cat << EOF
------ BEGIN LICENSE -----
-Member J2TeaM
-Single User License
-EA7E-1011316
-D7DA350E 1B8B0760 972F8B60 F3E64036
-B9B4E234 F356F38F 0AD1E3B7 0E9C5FAD
-FA0A2ABE 25F65BD8 D51458E5 3923CE80
-87428428 79079A01 AA69F319 A1AF29A4
-A684C2DC 0B1583D4 19CBD290 217618CD
-5653E0A0 BACE3948 BB2EE45E 422D2C87
-DD9AF44B 99C49590 D2DBDEE1 75860FD2
-8C8BB2AD B2ECE5A4 EFC08AF2 25A9B864
------- END LICENSE ------
-EOF
-echo "++++++++++ END KEY ++++++++++"
 
 # Autojump for zsh
 echo "Setting up Autojump..."
@@ -110,7 +87,7 @@ git clone --quiet https://github.com/scwuaptx/Pwngdb ~/tools/pwn/Pwngdb
 cd ~/tools/pwn/pwndbg
 ./setup.sh
 
-cp ./gdbinit ~/.gdbinit
+cp ${SAVED_PWD}/gdbinit ~/.gdbinit
 
 # gems: one_gadget, seccomp-tools
 echo "Setting up Gems..."
@@ -121,12 +98,12 @@ gem install seccomp-tools
 echo "Setting up ROPGadget..."
 git clone --quiet https://github.com/JonathanSalwan/ROPgadget ~/tools/pwn/ROPGadget
 
-# ghidra
-echo "Setting up Ghidra..."
-
-wget -P ~/tools/ https://ghidra-sre.org/ghidra_9.2.2_PUBLIC_20201229.zip
-cd ~/tools/
-unzip ./ghidra_9.2.2_PUBLIC_20201229.zip
+# # ghidra
+# echo "Setting up Ghidra..."
+# 
+# wget -P ~/tools/ https://ghidra-sre.org/ghidra_9.2.2_PUBLIC_20201229.zip
+# cd ~/tools/
+# unzip ./ghidra_9.2.2_PUBLIC_20201229.zip
 
 # crypto stuff
 echo "Setting up Crypto stuff..."
